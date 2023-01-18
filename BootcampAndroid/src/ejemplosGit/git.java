fb22012efd90fd8a1faf933f6ea909ddec40c199
LauTIPE <lautaro.efi@gmail.com>
2023-01-18 20:33:39 -0300

    hola mundo

---
+++ BootcampAndroid/src/ejemplosGit/git.java
@@ -0,0 +1,12 @@
+package ejemplosGit;
+
+public class git {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hola mundo");
+		System.out.println("adios");
+		
+	}
+
+}
